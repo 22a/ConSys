4a7888c57fc9aa2f362ec0c7d648bd5bda115a05
Peter Meehan <meehanp2@tcd.ie>
2015-02-04 03:28:53 +0000

    conducted some spring cleaning on the C

--- lab1/pi.c
+++ lab1/pi.c
@@ -1,9 +1,12 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
+//#include <time.h>
 
 #define MIN_ITER_PER_THREAD 2
+
+// Peter Meehan - 2015
+// check https://github.com/22a/ConSys/tree/master/lab1 for info on what this terrible code is for
 
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 int iter;
@@ -13,19 +16,20 @@ double pi;
 /* this function is run by the threads */
 void *calc(void* i_ptr)
 {
-	int i = *((int*)i_ptr);
-	int parity = (i+1)%2==0?1:-1;
+	int i = *((int*)i_ptr);				//void* to int casting magic
+	double parity = (double)((i+1)%2==0?1:-1);
 	int jump = numThreads*2;
 	double val = 0;
 	for(i = ((i+1)*2)+1; i < iter; i+=jump)
 	{
-		val+=(4.0/(double)i)*(double)parity;
+		val+=(4.0/(double)i)*parity;
 	}
-	//printf("new thread finished, total = %f\n", val);
+
+	//politely wait for mutex lock
 	pthread_mutex_lock(&mutex);
+	//"my precious" global variable
 	pi+=val;
 	pthread_mutex_unlock(&mutex);
-	/* the function must return something */
 	return NULL;
 }
 
@@ -41,19 +45,22 @@ int main(int argc, char* argv[])
 		fprintf(stderr, "Useage:\t./pi <number of threads> <number of iterations>\n");
 		exit(1);
 	}
-	numThreads = (int)atol(argv[1]);
-	iter = (int)atol(argv[2]);
+	numThreads = atoi(argv[1]);		//using atoi() here because no user will ever get to pass stupid args to this program
+	iter = atoi(argv[2]);			//only nice, clean, sensible scripts
+
+	//sanity checks to stop 2billion threads being created by accident
 	if(numThreads > iter*MIN_ITER_PER_THREAD)
 	{
 		fprintf(stderr, "You don't need %i threads to execute %i iterations! Make sure you have at least %i iterations per thread\n", numThreads, iter, MIN_ITER_PER_THREAD);
 		exit(1);
 	}
-	if(numThreads<1)
+	else if(numThreads<1)
 	{
 		fprintf(stderr, "You're going to need at least one thread.\n");
 		exit(1);
 	}
 
+	//iterative method for for when only one thread is selected, get rid of thread creation overhead entirely
 	if(numThreads==1)
 	{
 		int i;
@@ -64,14 +71,15 @@ int main(int argc, char* argv[])
 			pi+=((double)parity)*(4.0/(double)i);
 			parity = parity==1?-1:1;
 		}
-		//printf("pi = %f\n", pi);
 	}
 	else
 	{
-		pthread_t threads[numThreads];
+		pthread_t threads[numThreads];	//pthread instance variables
 		pi = 4.0;
 		int i;
-		int is[numThreads];
+		int is[numThreads];	//this array is needed as non volatile storage of the for loop incrementer
+
+		//thread creation
 		for(i = 0; i < numThreads; i++)
 		{
 			is[i] = i;
@@ -82,6 +90,7 @@ int main(int argc, char* argv[])
 			}
 		}
 
+		//waiting for threads to join
 		for(i = 0; i < numThreads; i++)
 		{
 			if(pthread_join(threads[i], NULL))
@@ -90,8 +99,8 @@ int main(int argc, char* argv[])
 				return 2;
 			}
 		}
-		//printf("pi = %f\n", pi);
 	}
+	//printf("pi = %f\n", pi);
 
 	// end = clock();
 	// time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
